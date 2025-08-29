57cedf382a2635e9d9ac445a0a7be1e3e23df0fa
shashika-himaruwani <sendhimaruwani@gmail.com>
2025-08-29 16:03:09 +0530

    Add reproductive, pregnancy, and postpartum dashboard UI designs

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "@/components/shared/container";
-
-export default function Page() {
-  return (
-    <Container title="Dashboard">
-      <h1>Dashboard</h1>
-    </Container>
-  );
-}
