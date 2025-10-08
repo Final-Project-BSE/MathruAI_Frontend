3c1c193131706dcf1084398a486f58f472b8d1bf
shashika-himaruwani <sendhimaruwani@gmail.com>
2025-10-08 17:01:24 +0530

    add router to logout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
